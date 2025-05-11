d600715ce377cdcf03a0c382ec99fa721966405b
hjjjjjjjjjjjnnnnnnnnnnnn <israelolalere2008@example.com>
2025-05-11 23:14:11 +0100

    Fix Vercel deployment: added vercel.json and updated vue.config.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/home',
     name: 'home',
     component: HomeView
   }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes
 })
 
